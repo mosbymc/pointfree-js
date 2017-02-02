0a5bd5b808fbb151cef50d2f471cb2259ab38404
mark.mosby <mark.mosby@ignitionone.com>
2017-02-02 13:41:54 -0500

    develop: added zip spec file

--- src/collation/zip.js
+++ src/collation/zip.js
@@ -1,7 +1,13 @@
+import { when, not, isArray } from '../functionalHelpers';
+
 function zip(source, collection, selector) {
     return function *zipIterator() {
         var res,
             idx = 0;
+        collection = when(not(isArray), Array.from, collection);
+
+        if (collection.length < 1) return [];
+
         for (let item of source) {
             if (idx > collection.length) return;
             res = selector(item, collection[idx]);
