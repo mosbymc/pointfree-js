cedee9aeb94a9460166e17f9d94858be52e91f19
mark.mosby <mark.mosby@ignitionone.com>
2017-02-09 06:30:46 -0500

    develop: updated deep flatten iterator to flatten data without causing the final form to be an array of arrays and to prevent call stack exceeded issues; finished up deep flatten spec file

--- src/projection/deepFlatten.js
+++ src/projection/deepFlatten.js
@@ -1,48 +1,41 @@
-import { when, wrap, isArray, isObject, not, and } from '../functionalHelpers';
+import { when, ifElse, wrap, isArray, isObject, not, and } from '../functionalHelpers';
 
-function deepFlatten(source, id) {
-    return function *deepFlattenIterator() {
-        var unyieldedData = [];
+function deepFlatten(source) {
+    return function *iterator() {
+        var unyieldedData = [],
+            res;
+
         for (let item of source) {
-            //var res = flattener(item);
+            res = flatteningFunc(item);
 
-            if (id) console.log(res);
-
-            if (isArray(res)) unyieldedData = unyieldedData.concat(res);
+            if (isArray(res)) unyieldedData = unyieldedData.concat(Array.prototype.concat.apply([], res));
             if (unyieldedData.length) yield unyieldedData.shift();
             else yield res;
         }
-
         while (unyieldedData.length) yield unyieldedData.shift();
     };
 }
 
-function flattener(item) {
-    return Array.prototype.concat.apply([], dataMapper(wrap(item)).map(function _flattenData(it) {
-        return when(isArray, flattener, it);
+function flatteningFunc(data) {
+    return ifElse(isArray, mapData, when(isObject, when(objectContainsOnlyArrays, getObjectKeysAsArray)), data);
+}
+
+function mapData(data) {
+    return Array.prototype.concat.apply([], data.map(function flattenArray(item) {
+        return flatteningFunc(item);
     }));
+}
+
+function getObjectKeysAsArray(data) {
+    return Object.keys(data).map(function _flattenKeys(key) {
+        return flatteningFunc(data[key]);
+    });
 }
 
 function objectContainsOnlyArrays(data) {
     return Object.keys(data).every(function _isMadeOfArrays(key) {
         return isArray(data[key]);
     });
-}
-
-function objectToArray(data) {
-    return Object.keys(data).map(function _transmogrify(key) {
-        return dataMapper(data[key]);
-    });
-}
-
-function dataMapper(data) {
-    return data.map(function _dataMapper(item) {
-        return when(isObjectButNotArray, when(objectContainsOnlyArrays, objectToArray), item);
-    });
-}
-
-function isObjectButNotArray(item) {
-    return and(not(isArray), isObject, item);
 }
 
 export { deepFlatten };
--- test/unit/projection/deepFlatten.spec.js
+++ test/unit/projection/deepFlatten.spec.js
@@ -1,6 +1,39 @@
 import { deepFlatten } from '../../../src/projection/deepFlatten';
 import { testData } from '../../testData';
 
+var multi_nested_data = [
+    [
+        {
+            a: [ [1, 2, 3], [4, 5, 6], [7, 8, 9] ],
+            b: [ [10, 11, 12], [13, 14, 15], [16, 17, 18] ],
+            c: [ [19, 20, 21], [22, 23, 24], [25, 26, 27] ],
+            d: [82, 83, 84, 85, 86]
+        },
+        {
+            a: [ [28, 29, 30], [31, 32, 33], [34, 35, 36] ],
+            b: [ [37, 38, 39], [40, 41, 42], [43, 44, 45] ],
+            c: [ [46, 47, 48], [49, 50, 51], [52, 53, 54] ],
+            d: [87, 88, 89, 90, 91]
+        },
+        {
+            a: [ [55, 56, 57], [58, 59, 60], [61, 62, 63] ],
+            b: [ [64, 65, 66], [67, 68, 69], [70, 71, 72] ],
+            c: [ [73, 74, 75], [76, 77, 78], [79, 80, 81] ],
+            d: [92, 93, 94, 95, 96]
+        },
+        97,
+        98,
+        99,
+        100,
+        101,
+        [102, 103, 104, 105, 106, [107, 108, 109, 110, 111, [112, 113, 114, 115, 116, 117, 118, 119, 120, 121] ] ]
+    ],
+    {
+        a: [122, 123, 124, 125, 126],
+        b: [127, [128, [129, [130, [131, [132, [133, [134, [135] ] ] ] ] ] ] ] ]
+    }
+];
+
 describe('Test deepFlatten', function testDeepFlatten() {
     it('should act as identity for already flat data', function testDeepFlattenWithFlatData() {
         var deepFlattenIterable = deepFlatten(testData.dataSource.data),
@@ -22,5 +55,23 @@ describe('Test deepFlatten', function testDeepFlatten() {
 
         flattenRes.should.have.lengthOf(testData.dataSource.data.length);
         flattenRes.should.eql(testData.dataSource.data);
+    });
+
+    it('should flatten many levels', function testDeepFlattenWithMultilevelNesting() {
+        var deepFlattenIterable = deepFlatten(multi_nested_data),
+            deepFlattenRes = Array.from(deepFlattenIterable());
+        deepFlattenRes.should.have.lengthOf(135);
+    });
+
+    it('should work with a generator', function testDeepFlattenWithGenerators() {
+        function *gen(data) {
+            for (let item of data)
+                yield item;
+        }
+
+        var deepFlattenIterable = deepFlatten(gen(multi_nested_data)),
+            deepFlattenRes = Array.from(deepFlattenIterable());
+
+        deepFlattenRes.should.have.lengthOf(135);
     });
 });
