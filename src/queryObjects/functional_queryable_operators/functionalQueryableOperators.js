b468e97dfc281fc7c37b691734021f3c8bba9742
mark.mosby <mark.mosby@ignitionone.com>
2017-04-17 06:03:05 -0400

    stream: added queryableZip

--- src/queryObjects/functional_queryable_operators/functionalQueryableOperators.js
+++ src/queryObjects/functional_queryable_operators/functionalQueryableOperators.js
@@ -1,11 +1,15 @@
 import { curry } from '../../functionalHelpers';
 
-var queryableMap = curry(function _queryableMap(queryable, func) {
+var queryableMap = curry(function _queryableMap(func, queryable) {
     return queryable.map(func);
 });
 
-var queryableFilter = curry(function _queryableFilter(queryable, predicate) {
+var queryableFilter = curry(function _queryableFilter(predicate, queryable) {
     return queryable.filter(predicate);
+});
+
+var queryableZip = curry(function _queryableZip(selector, enumerable, queryable) {
+    return queryable.zip(enumerable, selector);
 });
 
 export { queryableMap, queryableFilter };
