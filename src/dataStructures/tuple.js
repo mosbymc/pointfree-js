337e9d6dbd4b62a4e8eff02fe009bc01a1053518
mosbymc <mosbymc@users.noreply.github.com>
2018-07-06 07:47:20 -0400

    develop: Worked on tuple factory

--- src/dataStructures/tuple.js
+++ src/dataStructures/tuple.js
@@ -10,17 +10,57 @@ function Tuple(...args) {
     });
 
     args.forEach(function _setAccessorProps(arg, idx) {
-        Object.defineProperty(t, positions[idx], {
-            value: arg,
-            writable: false,
-            configurable: false
+        //Object.defineProperties doesn't accept 'positions[idx]' as a property name argument, so I have to
+        //define these two props separately using Object.defineProperty instead. Also, while unlikely, tuple
+        //only supports up to ten named properties, whereas I can index all properties regardless of how
+        //many there are.
+        Object.defineProperty(t, idx, {
+            get: function _getIdx() { return arg; }
         });
+
+        if (positions.length > idx) {
+            Object.defineProperty(t, positions[idx], {
+                get: function _getPlace() { return arg; }
+            });
+        }
     });
 
     return t;
 }
 
+Tuple.fromObject = function _fromObject(obj) {
+    var objectProps = Object.keys(obj),
+        t = Object.create(tuple, {
+            _source: {
+                value: objectProps.map(prop => obj[prop]),
+                writable: false,
+                configurable: false
+            }
+        });
+
+    objectProps.forEach(function _setAccessorProps(arg, idx) {
+        Object.defineProperty(t, idx, {
+            get: function _getIdx() { return obj[arg]; }
+        });
+
+        Object.defineProperty(t, arg, {
+            get: function _getProp() { return obj[arg]; }
+        });
+
+        if (positions.length > idx) {
+            Object.defineProperty(t, positions[idx], {
+                get: function _getPlace() { return obj[arg]; }
+            });
+        }
+    });
+
+    return t;
+};
+
 var tuple = {
+    concat: function _concat(other) {
+        return Tuple(this.source.concat(other.source));
+    },
     toArray: function _toArray() {
         return this._source;
     },
